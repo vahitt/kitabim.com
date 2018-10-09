66a0fd8272bb25478ab9864aabb2c5f54f652330
vahit <kuruosmanvahit@gmail.com>
2018-10-09 19:24:18 +0300

    content-top background eklendi

--- index.php
+++ index.php
@@ -5,45 +5,52 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>	
-	<header>
-		<div class="header-background">
-			<div class="container">		
-					<div class="row" style="margin-bottom: 25px;">
-						<div class="col-md-3 col-lg-3">
-							<a href="index.php" ><img src="images/logo.png"/></a>
-						</div>
-						<div class="col-md-3 offset-md-6 col-lg-3 offset-lg-6">
-							<div class="header-search"> 
-								<form class="example" action="#">
-									<input type="text" placeholder="Kitabını Bul" name="search" />
-									<button type="submit" >Git</button>	
-								</form>
+	<body>	
+		<header>
+			<div class="header-background">
+				<div class="container">		
+						<div class="row" style="margin-bottom: 25px;">
+							<div class="col-md-3 col-lg-3">
+								<a href="index.php" ><img src="images/logo.png"/></a>
+							</div>
+							<div class="col-md-3 offset-md-6 col-lg-3 offset-lg-6">
+								<div class="header-search"> 
+									<form class="example" action="#">
+										<input type="text" placeholder="Kitabını Bul" name="search" />
+										<button type="submit" >Git</button>	
+									</form>
+								</div>
 							</div>
 						</div>
-					</div>
 
-					<div class="row">
-						<div class="col-md-9 col-sm-12 col-lg-9">
-							<nav id="normal">
-								<a href="#" >AnaSayfa</a>
-								<a href="#" >Yeni</a>
-								<a href="#" >Takas</a>
-								<a href="#" >Bağış</a>
-								<a href="#" >Hakkımızda</a>	
-								<a href="#" >İletişim</a>
-							</nav>
-						</div>
+						<div class="row">
+							<div class="col-md-9 col-sm-12 col-lg-9">
+								<nav id="normal">
+									<a href="#" >AnaSayfa</a>
+									<a href="#" >Yeni</a>
+									<a href="#" >Takas</a>
+									<a href="#" >Bağış</a>
+									<a href="#" >Hakkımızda</a>	
+									<a href="#" >İletişim</a>
+								</nav>
+							</div>
 
-						<div class="col-md-3 col-sm-12 col-lg-3">
-							<nav id="gika">
-								<a href="#">Giriş Yap</a>
-								<a href="#">Kayıt Ol</a>
-							</nav>
+							<div class="col-md-3 col-sm-12 col-lg-3">
+								<nav id="gika">
+									<a href="#">Giriş Yap</a>
+									<a href="#">Kayıt Ol</a>
+								</nav>
+							</div>
 						</div>
-					</div>
+				</div>
 			</div>
-		</div>
-	</header>
-</body>
+		</header>
+		<div class="content-top-background">
+				<div class="container">
+					<div class="row">
+						
+					</div>
+				</div>
+			</div>
+	</body>
 </html>
