3c8c87465789f7fe009fc7fcc0e16aa5ccf5cd6d
vahit <kuruosmanvahit@gmail.com>
2018-10-15 15:06:20 +0300

    kod duzeni saglandi

---
+++ responsive.js
@@ -0,0 +1,37 @@
+document.getElementById('close').style.display = "none";
+function checkSize(size){
+	if(size.matches){
+		document.getElementById('Navbar__Items').style.display = "flex"; 				
+		document.getElementById('Navbar__Items_login').style.display = "flex";
+		
+	}
+	else{
+		document.getElementById('Navbar__Items').style.display = "flex"; 				
+		document.getElementById('Navbar__Items_login').style.display = "flex";		
+		document.getElementById('close').style.display = "flex";
+		document.getElementById('open').style.display = "none";
+	}
+}
+function openNav() {
+	var x = document.getElementById('open').style.display = "none";
+
+	if(x == "none"){
+		document.getElementById('Navbar__Items').style.display = "flex"; 				
+		document.getElementById('Navbar__Items_login').style.display = "flex";
+		 document.getElementById('close').style.display = "flex"
+	}
+}
+function closeNav() {
+	var y = document.getElementById('close').style.display;
+	if(y == "flex"){
+		document.getElementById('Navbar__Items').style.display = "none"; 				
+		document.getElementById('Navbar__Items_login').style.display = "none";
+		document.getElementById('close').style.display = "none";
+		document.getElementById('open').style.display = "flex";
+	}
+}
+var size = window.matchMedia("(min-width: 769px)");
+checkSize(size);
+size.addListener(checkSize)
+
+		
