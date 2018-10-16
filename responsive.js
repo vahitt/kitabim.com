975bb53af49aaddbd35bf6b5a0b759c99c25d8aa
vahit <kuruosmanvahit@gmail.com>
2018-10-16 16:14:24 +0300

    bi takim degisiklikler

--- responsive.js
+++ responsive.js
@@ -1,11 +1,12 @@
 document.getElementById('close').style.display = "none";
 function checkSize(size){
 	if(size.matches){
-		document.getElementById('Navbar__Items').style.display = "flex"; 				
-		
+		document.getElementById('Navbar__Items').style.display = "flex";
+		document.getElementById('nav').style.display = "flex";			
 	}
 	else{
-		document.getElementById('Navbar__Items').style.display = "flex"; 					
+		document.getElementById('Navbar__Items').style.display = "flex";
+		document.getElementById('nav').style.display = "flex";
 		document.getElementById('close').style.display = "flex";
 		document.getElementById('open').style.display = "none";
 	}
@@ -14,14 +15,16 @@ function openNav() {
 	var x = document.getElementById('open').style.display = "none";
 
 	if(x == "none"){
-		document.getElementById('Navbar__Items').style.display = "flex"; 				
-		 document.getElementById('close').style.display = "flex"
+		document.getElementById('Navbar__Items').style.display = "flex";
+		document.getElementById('nav').style.display = "flex"; 				
+		document.getElementById('close').style.display = "flex"
 	}
 }
 function closeNav() {
 	var y = document.getElementById('close').style.display;
 	if(y == "flex"){
-		document.getElementById('Navbar__Items').style.display = "none"; 				
+		document.getElementById('Navbar__Items').style.display = "none"; 
+		document.getElementById('nav').style.display = "none"; 
 		document.getElementById('close').style.display = "none";
 		document.getElementById('open').style.display = "flex";
 	}
